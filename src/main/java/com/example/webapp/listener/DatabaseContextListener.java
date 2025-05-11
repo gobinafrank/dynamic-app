99727af7472791390d2f4d535b61d45fdb5e6817
DobreTech-Repo <info@dobretech.com>
2025-05-11 08:24:04 -0500

    Create DatabaseContextListener.java

---
+++ src/main/java/com/example/webapp/listener/DatabaseContextListener.java
@@ -0,0 +1,25 @@
+package com.example.webapp.listener;
+
+import com.example.webapp.db.DatabaseConnection;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class DatabaseContextListener implements ServletContextListener {
+    private static final Logger logger = LogManager.getLogger(DatabaseContextListener.class);
+    
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        logger.info("Application context initialized, database connection pool is ready");
+    }
+    
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        logger.info("Application context being destroyed, closing database connection pool");
+        DatabaseConnection.closeDataSource();
+    }
+}
