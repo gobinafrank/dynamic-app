97e6589a1e9697d138c9b974618eb42f3a5d659c
DobreTech-Repo <info@dobretech.com>
2025-05-11 08:21:38 -0500

    Create HelloServlet.java

---
+++ src/main/java/com/example/webapp/HelloServlet.java
@@ -0,0 +1,36 @@
+package com.example.webapp;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private static final Logger logger = LogManager.getLogger(HelloServlet.class);
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        logger.info("HelloServlet doGet method called");
+        
+        String name = request.getParameter("name");
+        if (name == null || name.isEmpty()) {
+            name = "World";
+        }
+        
+        request.setAttribute("message", "Hello, " + name + "!");
+        request.getRequestDispatcher("/index.jsp").forward(request, response);
+    }
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
